737d2f56c5586f39712216d5e500ea237552c0e7
Petar Dodev <petar.dodev@gmail.com>
2014-04-24 14:40:19 +0400

    Playing around with the server

--- metro-social-observer/src/JsonServer.java
+++ metro-social-observer/src/JsonServer.java
@@ -1,6 +1,14 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
+import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class JsonServer {
@@ -19,7 +27,32 @@ public class JsonServer {
 	}
 	
 	public void stop() {
-		this._server.stop(0);
+		System.out.println("Starting server stopping procedure");
+		this._server.stop(1);
+		System.out.println("Ending server stopping procedure");
 	}
 	
+	private class RequestHandler implements HttpHandler {
+		public void handle(HttpExchange exchange) throws IOException {
+			System.out.println("hangling request");
+			String requestMethod = exchange.getRequestMethod();
+		    if (requestMethod.equalsIgnoreCase("GET")) {
+		    	Headers responseHeaders = exchange.getResponseHeaders();
+		    	responseHeaders.set("Content-Type", "text/plain");
+		    	exchange.sendResponseHeaders(200, 0);
+
+		    	OutputStream responseBody = exchange.getResponseBody();
+		    	Headers requestHeaders = exchange.getRequestHeaders();
+		    	Set<String> keySet = requestHeaders.keySet();
+		    	Iterator<String> iter = keySet.iterator();
+		    	while (iter.hasNext()) {
+		    		String key = iter.next();
+		    		List<String> values = requestHeaders.get(key);
+		    		String s = key + " = " + values.toString() + "\n";
+		    		responseBody.write(s.getBytes());
+		    	}
+		    	responseBody.close();
+		    }
+		}
+	}
 }
--- metro-social-observer/src/MetroSocialObserver.java
+++ metro-social-observer/src/MetroSocialObserver.java
@@ -1,15 +1,19 @@
-import java.io.IOException;
-
 public class MetroSocialObserver {
   public static void main(String[] args) {
-	  
-	  
 	  try {
 		  JsonServer server = new JsonServer();
-		  server.start();
 		  
 		  Thread exitHook = new ExitHookThread(server);
 		  Runtime.getRuntime().addShutdownHook(exitHook);
+		  
+		  server.start();
+		  System.out.println("Staring server and waiting 5sec");
+		  Thread.sleep(5000);
+		  
+		  System.out.println("Stopping server and waiting 5sec");
+		  server.stop();
+		  Thread.sleep(5000);
+		  System.out.println("And shutting down");
 	} catch (Exception e) {
 		  // TODO Auto-generated catch block
 		  e.printStackTrace();
