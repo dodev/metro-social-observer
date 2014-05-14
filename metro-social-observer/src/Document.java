4baaaed6f4f512ca7930e783a8b1751cd4b7549a
Petar Dodev <petar.dodev@gmail.com>
2014-05-14 13:50:59 +0400

    Document implementation

--- metro-social-observer/src/Document.java
+++ metro-social-observer/src/Document.java
@@ -7,5 +7,35 @@
  *
  */
 public class Document {
+	private String text;
+	private String[] keywords;
+	private int rating;
+	
+	public Document(String _text, String[] _keywords, int _rating) {
+		this.text = _text;
+		this.keywords = _keywords;
+		this.rating = _rating;
+	}
+
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * @return the keywords
+	 */
+	public String[] getKeywords() {
+		return keywords;
+	}
+
+	/**
+	 * @return the rating
+	 */
+	public int getRating() {
+		return rating;
+	}
 
 }
