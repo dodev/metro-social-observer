9b8a82ca10274749b5a663d88a218fef0b082b1e
Petar Dodev <petar.dodev@gmail.com>
2014-05-14 13:02:58 +0400

    Added document, iteration, iterationStorage, scheme and warning classes

---
+++ metro-social-observer/src/Iteration.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+
+/**
+ * @author dodev
+ *
+ */
+public class Iteration {
+	
+	private Object[] rawCollection;
+	/**
+	 * @return the rawCollection
+	 */
+	public Object[] getRawCollection() {
+		return rawCollection;
+	}
+
+	/**
+	 * @param rawCollection the rawCollection to set
+	 */
+	public void setRawCollection(Object[] rawCollection) {
+		this.rawCollection = rawCollection;
+	}
+
+	/**
+	 * @return the collection
+	 */
+	public Document[] getCollection() {
+		return collection;
+	}
+
+	/**
+	 * @param collection the collection to set
+	 */
+	public void setCollection(Document[] collection) {
+		this.collection = collection;
+	}
+
+	/**
+	 * @return the results
+	 */
+	public Warning[] getResults() {
+		return results;
+	}
+
+	/**
+	 * @param results the results to set
+	 */
+	public void setResults(Warning[] results) {
+		this.results = results;
+		this.timeFinished = System.currentTimeMillis() / 1000L;
+		Logger.notice("Iteration finished at " + this.timeFinished);
+	}
+
+	/**
+	 * @return the timeStarted
+	 */
+	public long getTimeStarted() {
+		return timeStarted;
+	}
+
+	/**
+	 * @return the timeFinished
+	 */
+	public long getTimeFinished() {
+		return timeFinished;
+	}
+
+	private Document[] collection;
+	private Warning[] results;
+	
+	private long timeStarted;
+	private long timeFinished;
+	
+	private Scheme scheme;
+	
+	public Iteration(Scheme sc) {
+		this.scheme = sc;
+		this.timeStarted = System.currentTimeMillis() / 1000L;
+		Logger.notice("Iteration started at " + this.timeStarted);
+	}
+
+}
---
+++ metro-social-observer/src/Warning.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+/**
+ * @author dodev
+ *
+ */
+public class Warning {
+
+}
